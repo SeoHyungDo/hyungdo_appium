6bcb201adf6fdbd982ddd03b0e4b019c9d4251e6
SeoHyungDo <tjg106@gmail.com>
2025-12-05 02:04:50 +0900

    1205 update

--- root_obj/main_page_obj.py
+++ root_obj/main_page_obj.py
@@ -1,20 +1,15 @@
-from http.client import responses
-from selenium.webdriver.common.keys import Keys
 from appium.webdriver.common.appiumby import AppiumBy
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from root_obj.login_page_obj import swaglabs_home
-import time
-from utility.wait import Wait
+
 
 class swaglabs_main_page:
 
-    def __init__(self, driver) :
+    def __init__(self, driver):
         self.driver = driver
 
-        self.hamburger_menu_locator = (AppiumBy.XPATH,'//android.view.ViewGroup[@content-desc="test-Menu"]/android.view.ViewGroup')
+        self.hamburger_menu_locator = (
+        AppiumBy.XPATH, '//android.view.ViewGroup[@content-desc="test-Menu"]/android.view.ViewGroup')
         self.top_logo_locator = (AppiumBy.XPATH,
-                        '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.ImageView[2]')
+                                 '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.ImageView[2]')
 
         self.input_Username_locator = (AppiumBy.ACCESSIBILITY_ID, "test-Username")
         self.input_pw_locator = (AppiumBy.ACCESSIBILITY_ID, "test-Password")
@@ -31,9 +26,220 @@ class swaglabs_main_page:
         self.menu_reset_locator = (AppiumBy.XPATH, '//android.widget.TextView[@text="RESET APP STATE"]')
         self.cart_locator = (AppiumBy.XPATH, '//android.view.ViewGroup[@content-desc="test-Cart drop zone"]')
         self.product_text = (AppiumBy.XPATH, '//android.widget.TextView[@text="PRODUCTS"]')
-        self.text_modal_selector = (AppiumBy.XPATH, '//android.view.ViewGroup[@content-desc="test-Modal Selector Button"]')
+        self.text_modal_selector = (
+        AppiumBy.XPATH, '//android.view.ViewGroup[@content-desc="test-Modal Selector Button"]')
 
-        self.first_product_image = (AppiumBy.XPATH, '(//android.view.ViewGroup[@content-desc="test-Item"])[1]/android.view.ViewGroup/android.widget.ImageView')
+
+        self.first_product_image = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Backpack"]]'
+            '//android.widget.ImageView'
+        )
+        self.first_product_name = (
+            AppiumBy.XPATH,
+            '//android.widget.TextView[@content-desc="test-Item title" and @text="Sauce Labs Backpack"]'
+        )
+        self.first_product_price = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Backpack"]]'
+            '//android.widget.TextView[@content-desc="test-Price"]'
+        )
+        self.first_product_add_to_cart_button = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Backpack"]]'
+            '//android.widget.TextView[@text="ADD TO CART"]'
+        )
+
+
+        self.second_product_image = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Bike Light"]]'
+            '//android.widget.ImageView'
+        )
+        self.second_product_name = (
+            AppiumBy.XPATH,
+            '//android.widget.TextView[@content-desc="test-Item title" and @text="Sauce Labs Bike Light"]'
+        )
+        self.second_product_price = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Bike Light"]]'
+            '//android.widget.TextView[@content-desc="test-Price"]'
+        )
+        self.second_product_add_to_cart_button = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Bike Light"]]'
+            '//android.widget.TextView[@text="ADD TO CART"]'
+        )
+
+        self.third_product_image = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Bolt T-Shirt"]]'
+            '//android.widget.ImageView'
+        )
+        self.third_product_name = (
+            AppiumBy.XPATH,
+            '//android.widget.TextView[@content-desc="test-Item title" and @text="Sauce Labs Bolt T-Shirt"]'
+        )
+        self.third_product_price = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Bolt T-Shirt"]]'
+            '//android.widget.TextView[@content-desc="test-Price"]'
+        )
+        self.third_product_add_to_cart_button = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Bolt T-Shirt"]]'
+            '//android.widget.TextView[@text="ADD TO CART"]'
+        )
+
+        self.fourth_product_image = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Fleece Jacket"]]'
+            '//android.widget.ImageView'
+        )
+        self.fourth_product_name = (
+            AppiumBy.XPATH,
+            '//android.widget.TextView[@content-desc="test-Item title" and @text="Sauce Labs Fleece Jacket"]'
+        )
+        self.fourth_product_price = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Fleece Jacket"]]'
+            '//android.widget.TextView[@content-desc="test-Price"]'
+        )
+        self.fourth_product_add_to_cart_button = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Fleece Jacket"]]'
+            '//android.widget.TextView[@text="ADD TO CART"]'
+        )
+
+        self.fifth_product_image = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Onesie"]]'
+            '//android.widget.ImageView'
+        )
+        self.fifth_product_name = (
+            AppiumBy.XPATH,
+            '//android.widget.TextView[@content-desc="test-Item title" and @text="Sauce Labs Onesie"]'
+        )
+        self.fifth_product_price = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Onesie"]]'
+            '//android.widget.TextView[@content-desc="test-Price"]'
+        )
+        self.fifth_product_add_to_cart_button = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Sauce Labs Onesie"]]'
+            '//android.widget.TextView[@text="ADD TO CART"]'
+        )
+
+        self.sixth_product_image = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Test.allTheThings() T-Shirt (Red)"]]'
+            '//android.widget.ImageView'
+        )
+        self.sixth_product_name = (
+            AppiumBy.XPATH,
+            '//android.widget.TextView[@content-desc="test-Item title" and @text="Test.allTheThings() T-Shirt (Red)"]'
+        )
+        self.sixth_product_price = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Test.allTheThings() T-Shirt (Red)"]]'
+            '//android.widget.TextView[@content-desc="test-Price"]'
+        )
+        self.sixth_product_add_to_cart_button = (
+            AppiumBy.XPATH,
+            '//android.view.ViewGroup[@content-desc="test-Item"]'
+            '[.//android.widget.TextView[@text="Test.allTheThings() T-Shirt (Red)"]]'
+            '//android.widget.TextView[@text="ADD TO CART"]'
+        )
+
+    def scroll_until_text(self, text):
+        self.driver.find_element(
+            AppiumBy.ANDROID_UIAUTOMATOR,
+            f'new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text("{text}"))'
+        )
+
+    def scroll_until_product_image(self, product_name: str):
+        return self.driver.find_element(
+            AppiumBy.ANDROID_UIAUTOMATOR,
+            f'new UiScrollable(new UiSelector().scrollable(true))'
+            f'.scrollIntoView(new UiSelector().text("{product_name}"))'
+        ).find_element(
+            AppiumBy.XPATH,
+            '../../..//android.widget.ImageView'
+        )
+
+    def get_add_to_cart_button_by_product(self, product_name: str):
+        self.scroll_until_text(product_name)
+        return self.driver.find_element(
+            AppiumBy.XPATH,
+            f'//android.view.ViewGroup[@content-desc="test-Item"]'
+            f'[.//android.widget.TextView[@text="{product_name}"]]'
+            f'//android.widget.TextView[@text="ADD TO CART"]'
+        )
+
+    def product_price_text(self, product_name: str) -> str:
+        self.scroll_until_text(product_name)
+        el = self.driver.find_element(
+            AppiumBy.XPATH,
+            f'//android.view.ViewGroup[@content-desc="test-Item"]'
+            f'[.//android.widget.TextView[@text="{product_name}"]]'
+            f'//android.widget.TextView[@content-desc="test-Price"]'
+        )
+        return el.text
+
+    def product_card(self, product_name: str):
+        self.scroll_until_text(product_name)
+        return self.driver.find_element(
+            AppiumBy.XPATH,
+            f'//android.view.ViewGroup[@content-desc="test-Item"]'
+            f'[.//android.widget.TextView[@content-desc="test-Item title" and @text="{product_name}"]]'
+        )
+
+    def _get_product_image_by_name(self, product_name: str):
+        """
+        상품 이름 TextView를 기준으로 위로 올라가면서
+        가까운 조상 뷰들 안에서 ImageView를 찾아 반환한다. 구조가 조금 달라도 웬만하면 커버되도록 만든 안전형 헬퍼.
+        """
+        self.scroll_until_text(product_name)
+
+        # 제목 TextView 요소 찾기 (이건 이미 잘 동작하는 패턴)
+        title_el = self.driver.find_element(
+            AppiumBy.XPATH,
+            f'//android.widget.TextView[@content-desc="test-Item title" and @text="{product_name}"]'
+        )
+
+        # 제목 기준으로 위로 최대 4단계까지 올라가면서, 그 조상/조상들 아래에서 ImageView 찾기 시도
+        current = title_el
+        for _ in range(4):
+            try:
+                # 현재 노드(or 조상) 아래에서 ImageView를 찾아본다.
+                img = current.find_element(AppiumBy.CLASS_NAME, 'android.widget.ImageView')
+                return img
+            except Exception:
+                # 못 찾으면 한 단계 위로 올라감
+                try:
+                    current = current.find_element(AppiumBy.XPATH, '..')
+                except Exception:
+                    break
+
+        raise Exception(f'상품 "{product_name}" 에 대한 이미지를 찾지 못했습니다.')
 
     @property
     def input_Username(self):
@@ -142,8 +348,12 @@ class swaglabs_main_page:
         return self.driver.find_element(*self.cart_locator)
 
     def cart_button_click(self):
-        if self.driver.find_element(*self.cart_button) :
-            self.cart_button.click()
+        try:
+            btn = self.cart_button
+            if btn:
+                btn.click()
+        except Exception:
+            pass
 
     @property
     def product_text_check(self):
@@ -154,10 +364,164 @@ class swaglabs_main_page:
         return self.driver.find_element(*self.text_modal_selector)
 
     def text_modal_selector_button_click(self):
-        if self.driver.find_element(*self.text_modal_selector_button) :
-            self.text_modal_selector_button.click()
-
+        try:
+            btn = self.text_modal_selector_button
+            if btn:
+                btn.click()
+        except Exception:
+            pass
 
     @property
     def first_product_image_displayed(self):
-        return self.driver.find_element(*self.first_product_image)
+        return self.product_card("Sauce Labs Backpack")
+
+    @property
+    def first_product_image_click(self):
+        return self.first_product_image_displayed.click
+
+    @property
+    def first_product_name_text(self):
+        return self.driver.find_element(*self.first_product_name).text
+
+    @property
+    def first_product_price_text(self):
+        return self.driver.find_element(*self.first_product_price).text
+
+    @property
+    def first_product_add_to_cart_button_click_obj(self):
+        return self.driver.find_element(*self.first_product_add_to_cart_button)
+
+    @property
+    def first_product_add_to_cart_button_click(self):
+        return self.first_product_add_to_cart_button_click_obj.click()
+
+# Second
+
+    @property
+    def second_product_image_displayed(self):
+        return self.product_card("Sauce Labs Bike Light")
+
+    @property
+    def second_product_image_click(self):
+        return self.second_product_image_displayed.click
+
+    @property
+    def second_product_name_text(self):
+        return self.driver.find_element(*self.second_product_name).text
+
+    @property
+    def second_product_price_text(self):
+        return self.driver.find_element(*self.second_product_price).text
+
+    @property
+    def second_product_add_to_cart_button_click_obj(self):
+        return self.driver.find_element(*self.second_product_add_to_cart_button)
+
+    @property
+    def second_product_add_to_cart_button_click(self):
+        return self.second_product_add_to_cart_button_click_obj.click()
+
+
+# third
+    @property
+    def third_product_image_displayed(self):
+        return self.product_card("Sauce Labs Bolt T-Shirt")
+
+    @property
+    def third_product_image_click(self):
+        return self.third_product_image_displayed.click
+
+    @property
+    def third_product_name_text(self):
+        return self.driver.find_element(*self.third_product_name).text
+
+    @property
+    def third_product_price_text(self):
+        return self.driver.find_element(*self.third_product_price).text
+
+    @property
+    def third_product_add_to_cart_button_click_obj(self):
+        return self.driver.find_element(*self.third_product_add_to_cart_button)
+
+    @property
+    def third_product_add_to_cart_button_click(self):
+        return self.third_product_add_to_cart_button_click_obj.click()
+
+# fourth
+# 3, 4번만 같은 방식으로 했을때 에러가 나서 바꿔 놓음
+    @property
+    def fourth_product_image_displayed(self):
+        return self.product_card("Sauce Labs Fleece Jacket")
+
+    @property
+    def fourth_product_image_click(self):
+        return self.fourth_product_image_displayed.click
+
+    @property
+    def fourth_product_name_text(self):
+        return self.driver.find_element(*self.fourth_product_name).text
+
+    @property
+    def fourth_product_price_text(self):
+        return self.driver.find_element(*self.fourth_product_price).text
+
+    @property
+    def fourth_product_add_to_cart_button_click_obj(self):
+        return self.driver.find_element(*self.fourth_product_add_to_cart_button)
+
+    @property
+    def fourth_product_add_to_cart_button_click(self):
+        return self.fourth_product_add_to_cart_button_click_obj.click()
+
+
+# fifth
+    @property
+    def fifth_product_image_displayed(self):
+        return self.product_card("Sauce Labs Onesie")
+
+    @property
+    def fifth_product_image_click(self):
+        return self.fifth_product_image_displayed.click
+
+    @property
+    def fifth_product_name_text(self):
+        return self.driver.find_element(*self.fifth_product_name).text
+
+    @property
+    def fifth_product_price_text(self):
+        return self.driver.find_element(*self.fifth_product_price).text
+
+    @property
+    def fifth_product_add_to_cart_button_click_obj(self):
+        return self.driver.find_element(*self.fifth_product_add_to_cart_button)
+
+    @property
+    def fifth_product_add_to_cart_button_click(self):
+        return self.fifth_product_add_to_cart_button_click_obj.click()
+
+
+
+# sixth
+    @property
+    def sixth_product_image_displayed(self):
+        return self.product_card("Test.allTheThings() T-Shirt (Red)")
+
+    @property
+    def sixth_product_image_click(self):
+        return self.sixth_product_image_displayed.click
+
+    @property
+    def sixth_product_name_text(self):
+        return self.driver.find_element(*self.sixth_product_name).text
+
+    @property
+    def sixth_product_price_text(self):
+        return self.driver.find_element(*self.sixth_product_price).text
+
+    @property
+    def sixth_product_add_to_cart_button_click_obj(self):
+        return self.driver.find_element(*self.sixth_product_add_to_cart_button)
+
+    @property
+    def sixth_product_add_to_cart_button_click(self):
+        return self.sixth_product_add_to_cart_button_click_obj.click()
--- test_action/test_mainpage.py
+++ test_action/test_mainpage.py
@@ -11,6 +11,7 @@ from root_obj.main_page_obj import swaglabs_main_page
 
 class TestMainpage(passclass):
 
+
     def test_login_hamburger_menu_check(self):
         self.swag_home.login()
         assert self.swag_home.hamburger_menu_icon.is_displayed()
@@ -41,6 +42,7 @@ class TestMainpage(passclass):
         assert self.swag_home.menu_reset_text == "RESET APP STATE"
 
     def test_top_logo_display(self):
+        self.swag_home.close_menu_if_open()
         assert self.swag_home.top_logo.is_displayed()
 
     def test_cart_button_display(self):
@@ -52,5 +54,114 @@ class TestMainpage(passclass):
     def test_text_modal_selector_button_check(self):
         assert self.swag_home.text_modal_selector_button.is_displayed()
 
+    # -----------------------------
+    # 1st product - Sauce Labs Backpack
+    # -----------------------------
     def test_first_product_image_display(self):
+        self.swag_home.scroll_until_text("Sauce Labs Backpack")
         assert self.swag_home.first_product_image_displayed.is_displayed()
+
+    def test_first_product_name_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Backpack")
+        assert self.swag_home.first_product_name_text == "Sauce Labs Backpack"
+
+    def test_first_product_price_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Backpack")
+        assert self.swag_home.first_product_price_text == "$29.99"
+
+    def test_first_product_add_to_cart_button_text_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Backpack")
+        assert self.swag_home.first_product_add_to_cart_button_click_obj.text == "ADD TO CART"
+
+    # -----------------------------
+    # 2nd product - Sauce Labs Bike Light
+    # -----------------------------
+    def test_second_product_image_display(self):
+        self.swag_home.scroll_until_text("Sauce Labs Bike Light")
+        assert self.swag_home.second_product_image_displayed.is_displayed()
+
+    def test_second_product_name_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Bike Light")
+        assert self.swag_home.second_product_name_text == "Sauce Labs Bike Light"
+
+    def test_second_product_price_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Bike Light")
+        assert self.swag_home.second_product_price_text == "$9.99"
+
+    def test_second_product_add_to_cart_button_text_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Bike Light")
+        assert self.swag_home.second_product_add_to_cart_button_click_obj.text == "ADD TO CART"
+
+    # -----------------------------
+    # 3rd product - Sauce Labs Bolt T-Shirt
+    # -----------------------------
+    def test_third_product_image_display(self):
+        self.swag_home.scroll_until_text("Sauce Labs Bolt T-Shirt")
+        assert self.swag_home.third_product_image_displayed.is_displayed()
+
+    def test_third_product_name_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Bolt T-Shirt")
+        assert self.swag_home.third_product_name_text == "Sauce Labs Bolt T-Shirt"
+
+    def test_third_product_price_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Bolt T-Shirt")
+        assert self.swag_home.third_product_price_text == "$15.99"
+
+    def test_third_product_add_to_cart_button_text_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Bolt T-Shirt")
+        assert self.swag_home.third_product_add_to_cart_button_click_obj.text == "ADD TO CART"
+
+    # -----------------------------
+    # 4th product - Sauce Labs Fleece Jacket
+    # -----------------------------
+    def test_fourth_product_image_display(self):
+        self.swag_home.scroll_until_text("Sauce Labs Fleece Jacket")
+        assert self.swag_home.fourth_product_image_displayed.is_displayed()
+
+    def test_fourth_product_name_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Fleece Jacket")
+        assert self.swag_home.fourth_product_name_text == "Sauce Labs Fleece Jacket"
+
+    def test_fourth_product_price_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Fleece Jacket")
+        assert self.swag_home.fourth_product_price_text == "$49.99"
+
+    def test_fourth_product_add_to_cart_button_text_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Fleece Jacket")
+        assert self.swag_home.fourth_product_add_to_cart_button_click_obj.text == "ADD TO CART"
+
+    # -----------------------------
+    # 5th product - Sauce Labs Onesie
+    # -----------------------------
+    def test_fifth_product_image_display(self):
+        self.swag_home.scroll_until_text("Sauce Labs Onesie")
+        assert self.swag_home.fifth_product_image_displayed.is_displayed()
+
+    def test_fifth_product_name_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Onesie")
+        assert self.swag_home.fifth_product_name_text == "Sauce Labs Onesie"
+
+    def test_fifth_product_price_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Onesie")
+        assert self.swag_home.fifth_product_price_text == "$7.99"
+
+    def test_fifth_product_add_to_cart_button_text_check(self):
+        self.swag_home.scroll_until_text("Sauce Labs Onesie")
+        assert self.swag_home.fifth_product_add_to_cart_button_click_obj.text == "ADD TO CART"
+
+
+    def test_sixth_product_image_display(self):
+        self.swag_home.scroll_until_text("Test.allTheThings() T-Shirt (Red)")
+        assert self.swag_home.sixth_product_image_displayed.is_displayed()
+
+    def test_sixth_product_name_check(self):
+        self.swag_home.scroll_until_text("Test.allTheThings() T-Shirt (Red)")
+        assert self.swag_home.sixth_product_name_text == "Test.allTheThings() T-Shirt (Red)"
+
+    def test_sixth_product_price_check(self):
+        self.swag_home.scroll_until_text("Test.allTheThings() T-Shirt (Red)")
+        assert self.swag_home.sixth_product_price_text == "$15.99"
+
+    def test_sixth_product_add_to_cart_button_text_check(self):
+        self.swag_home.scroll_until_text("Test.allTheThings() T-Shirt (Red)")
+        assert self.swag_home.sixth_product_add_to_cart_button_click_obj.text == "ADD TO CART"
