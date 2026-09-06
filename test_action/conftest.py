39c68e37cafecbce43a46ef029612632cc426a1d
seohyungdo <tjg106@gmail.com>
2026-09-06 10:47:05 +0900

    기기 특정 udid 삭제, 연결된 기기 자동 탐색 . 자동화 실행하도록 수정

--- test_action/conftest.py
+++ test_action/conftest.py
@@ -5,13 +5,38 @@ from root_obj.login_page_obj import swaglabs_home
 from root_obj.main_page_obj import swaglabs_main_page
 from utility.wait import Wait
 from root_obj.cart_page_obj import cart_page
+import subprocess # 연결기기 탐색
+
+def get_connected_device(): # 연결된 기기 자동 탐색
+    result = subprocess.run(
+        ["adb", "devices"],
+        capture_output=True,
+        text=True,
+        check=True
+    )
+
+    devices = []
+
+    for line in result.stdout.splitlines()[1:]:
+        if "\tdevice" in line:
+            devices.append(line.split("\t")[0])
+
+    if not devices:
+        raise RuntimeError("연결된 Android 기기가 없습니다.")
+
+    if len(devices) > 1:
+        raise RuntimeError(
+            f"Android 기기가 여러 대 연결되어 있습니다: {devices}"
+        )
+
+    return devices[0]
 
 @pytest.fixture(scope="class")
 def setup(request):
     options = UiAutomator2Options()
     options.platform_name = "Android"
     options.automation_name = "UIAutomator2"
-    options.udid = "R3CW107YQMB"
+    options.udid = get_connected_device() # 연결된 기기 자동 탐색
 
     options.app_package = "com.swaglabsmobileapp"
     options.app_activity = "com.swaglabsmobileapp.SplashActivity"
