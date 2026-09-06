5a2f115fc9fcc4e0626834d8b6ad780794f83df8
seohyungdo <tjg106@gmail.com>
2026-09-06 10:54:39 +0900

    실패시 프로젝트 루트에 자동 스크린샷 구현

--- test_action/conftest.py
+++ test_action/conftest.py
@@ -6,6 +6,8 @@ from root_obj.main_page_obj import swaglabs_main_page
 from utility.wait import Wait
 from root_obj.cart_page_obj import cart_page
 import subprocess # 연결기기 탐색
+from datetime import datetime # 스크린샷 자동으로 남기기
+import os
 
 def get_connected_device(): # 연결된 기기 자동 탐색
     result = subprocess.run(
@@ -30,6 +32,30 @@ def get_connected_device(): # 연결된 기기 자동 탐색
         )
 
     return devices[0]
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    # 실제 테스트 수행(call) 단계에서 실패했을 때만 캡처
+    if report.when == "call" and report.failed:
+
+        driver = None
+
+        # 현재 테스트 클래스에 저장된 Appium driver 가져오기
+        if item.instance and hasattr(item.instance, "driver"):
+            driver = item.instance.driver
+
+        if driver:
+            os.makedirs("screenshots", exist_ok=True)
+
+            now = datetime.now().strftime("%Y%m%d_%H%M%S")
+            file_name = f"screenshots/{item.name}_{now}.png"
+
+            driver.save_screenshot(file_name)
+
+            print(f"\n📸 실패 스크린샷 저장: {file_name}")
 
 @pytest.fixture(scope="class")
 def setup(request):
